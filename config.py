80b4bc898218c2ebe09d7bca51ac393d0a022564
cimraankhaan1 <103635804+cimraankhaan1@users.noreply.github.com>
2024-12-02 02:10:28 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,24 +3,24 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "27672739"))
+API_HASH = os.environ.get("API_HASH", "f48d0da94f2622364ab24eb0d911903c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8088534251:AAFwOd8-S6NCucBb-unMGh4QZKYabU3Sq7g")
+ADMIN = int(os.environ.get("ADMIN", "5069198943"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzcBuwwbs6gs9lNA6uVW1g1BZkZnxKLZp2gwSJ-HKV2niepAY3SQGFpNis65WKljBhWyWC_nIt_hx7UFkB1PBnY7VY6V9h5FOiaQse3UPGtC2pMJkZORxqKjjk49TEj3GhnqJJ3GlKDRSz8L9DQ8Aj8gvPs-QmW1c8CutwaqX76fducbGLwJ5myImTJGKb83rnqksTF7TGAZS3RD9SJhAnqlM3pdFCvLndsJle_PXJ5cpZFQHfmpDbpByPYDnnuA4hZ_ul4F0nTs5p__hgPypvLTk4b_eiwsIEYEHViKZ3eKhAlbY5gnVczCWFOEuAv77dB048X1tj2ZFfFvqgna5wOgB7s=")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002318997215")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002434765617"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://khaanbarwaani:qL47JyUNrzSRp2Aj@cluster0.qgucx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "khaanbarwaani")
 
 
 # Other Variables Config
